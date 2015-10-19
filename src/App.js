33b93672d429dec0a495f4ae6518b0cdb08f1724
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2015-10-19 21:53:47 -0200

    Component nomenclature

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 
 let React = require('react'); // eslint-disable-line
 let ReactDOM = require('react-dom');
-let MenuComponent = require('./components/Menu.react');
-let HeaderComponent = require('./components/Header.react');
+let Menu = require('./components/Menu.react');
+let Header = require('./components/Header.react');
 let Router = require('./Router');
 let Storage = require('./Storage');
 let Migration = require('./Migration');
@@ -12,5 +12,5 @@ Storage.init();
 Router.init();
 Migration.init();
 
-ReactDOM.render(<HeaderComponent />, document.querySelector('header'));
-ReactDOM.render(<MenuComponent />, document.querySelector('.menu'));
+ReactDOM.render(<Header />, document.querySelector('header'));
+ReactDOM.render(<Menu />, document.querySelector('.menu'));
