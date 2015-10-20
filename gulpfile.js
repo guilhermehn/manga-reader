5f13b4bdf3fb5bd3a8875509b8373e62764614af
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2015-10-20 19:59:26 -0200

    Proper gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var DEST = '../gallery/public';
 
 gulp.task('css', function() {
-  return gulp.src('assets/css/main.styl')
+  return gulp.src('./styl/main.styl')
     .pipe(plugins.plumber())
     .pipe(plugins.stylus())
     .pipe(plugins.myth())
     .pipe(plugins.autoprefixer({
       browsers: ['last 5 Chrome versions']
     }))
-    .pipe(gulp.dest(DEST));
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('css:watch', function() {
-  gulp.watch('assets/css/**/*.styl', ['css']);
+  gulp.watch('./styl/*.styl', ['css']);
 });
 
 gulp.task('default', ['css']);
