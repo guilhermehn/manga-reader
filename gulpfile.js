f106c832db36ddb5730330fa4fb3bf726c79c7a8
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2016-03-22 12:11:23 -0300

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var gulp = require('gulp')
+var plugins = require('gulp-load-plugins')()
 
 gulp.task('css', function() {
   return gulp.src('./styl/main.styl')
     .pipe(plugins.plumber())
+    .pipe(plugins.sourcemaps.init())
     .pipe(plugins.stylus())
     .pipe(plugins.myth())
     .pipe(plugins.autoprefixer({
       browsers: ['last 5 Chrome versions']
     }))
-    .pipe(gulp.dest('./build/'));
-});
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest('./build/'))
+})
 
-gulp.task('css:watch', function() {
-  gulp.watch('./styl/*.styl', ['css']);
-});
+gulp.task('css:watch', ['css'], function() {
+  gulp.watch('./styl/*.styl', ['css'])
+})
 
 gulp.task('watch-build', function() {
   return gulp.src('./build/*')
-    .pipe(plugins.livereload());
-});
+    .pipe(plugins.livereload())
+})
 
 gulp.task('watch', function() {
-  plugins.livereload.listen();
-  gulp.watch('./build/*', ['watch-build']);
-  gulp.start('css:watch');
-});
+  plugins.livereload.listen()
+  gulp.watch('./build/*', ['watch-build'])
+  gulp.start('css:watch')
+})
 
-gulp.task('default', ['css']);
+gulp.task('default', ['css'])
